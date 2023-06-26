ea49bf5eb2741baec453aaf56e0121a0253b32a8
Sakib Ahamed <35700378+zsxkib@users.noreply.github.com>
2023-06-26 21:43:53 +0100

    shell script so i don't forget what to cog run

---
+++ cog-run-these.sh
@@ -0,0 +1,4 @@
+cog run script/download-weights
+cog run wget https://huggingface.co/Sosaka/Illuminati/resolve/main/embeddings/nfixer.pt
+cog run wget https://huggingface.co/Sosaka/Illuminati/resolve/main/embeddings/nartfixer.pt
+cog run wget https://huggingface.co/Sosaka/Illuminati/resolve/main/embeddings/nrealfixer.pt
